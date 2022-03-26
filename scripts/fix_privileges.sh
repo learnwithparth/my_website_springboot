7d02f32805d99b84db4f3ea87059c887f7fffaf9
learnwithparth <68218484+learnwithparth@users.noreply.github.com>
2022-03-26 12:55:43 +0530

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/* jar
-chmod +x /home/ec2-user/server_start.sh
-chmod +x /home/ec2-user/server_stop.sh
+chmod +x /home/ec2-user/scripts/server_start.sh
+chmod +x /home/ec2-user/scripts/server_stop.sh
